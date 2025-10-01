215902ee92bb20a02b4184f09615a81a3ecae5ba
TomasCzC <tomasbecak@atlas.cz>
2025-10-01 09:49:50 +0200

    Update LMS_&_RLS.py

--- Python/LMS_&_RLS.py
+++ Python/LMS_&_RLS.py
@@ -9,9 +9,10 @@ s = np.sin(2*np.pi*f*t) #Signál
 n = np.random.normal(0, 0.1, len(t)) #Šum (Noise)
 x = s + n
 
-plt.plot(t, x, label='Sinus + šum')
+plt.plot(t, x)
 plt.xlabel("čas [s]")
 plt.ylabel("Ampl")
-#plt.plot(t, y, label='filtrováno')
+plt.title("Sinus + šum")
+#plt.plot(t, y)
 plt.legend()
 plt.show()
