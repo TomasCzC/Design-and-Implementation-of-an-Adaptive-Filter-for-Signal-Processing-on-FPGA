ba718819c46f8a5bf74c49876f9f19ae00dfc5a4
TomasCzC <tomasbecak@atlas.cz>
2025-11-25 14:08:48 +0100

    Update fft_utils.py

--- adaptive-filters-gui/src/filters/fft_utils.py
+++ adaptive-filters-gui/src/filters/fft_utils.py
@@ -1,1 +1,13 @@
+import numpy as np
+from .safety import SAFE_MAX
 
+def fft_mag(x, fs):
+    x = np.nan_to_num(x, nan=0.0, posinf=SAFE_MAX, neginf=-SAFE_MAX)
+    if x.size == 0:
+        return np.array([]), np.array([])
+
+    N = len(x)
+    f = np.fft.rfftfreq(N, 1.0 / fs)
+    X = np.fft.rfft(x) / max(1, N)
+    X = np.nan_to_num(X)
+    return f, np.abs(X) + 1e-15
