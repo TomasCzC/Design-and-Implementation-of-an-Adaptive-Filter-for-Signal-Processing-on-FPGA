361546475fd5f51476b588a4bf8805d8ca1ab782
TomasCzC <tomasbecak@atlas.cz>
2025-11-25 14:25:12 +0100

    Update __init__.py

--- adaptive-filters-gui/src/filters/__init__.py
+++ adaptive-filters-gui/src/filters/__init__.py
@@ -1,1 +1,9 @@
+from .safety import (
+    SAFE_MAX, SAFE_SQ_MAX, SAFE_MIN_POS,
+    clamp_array, safe_square, safe_log10_of_square, is_diverged
+)
 
+from .signal_generation import make_signals, hist_input
+from .metrics import compute_metrics, moving_avg
+from .fft_utils import fft_mag
+from .filter_runner import run_padasip_filter, enforce_runtime_stability
